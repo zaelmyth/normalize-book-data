c06170eb6d181776da68f1b38897848c951695d1
Flavius Dranca <flavius.dranca@gmail.com>
2025-03-08 18:44:25 +0200

    Refactor main code

--- main.py
+++ main.py
@@ -8,47 +8,14 @@ import normalizer
 def main():
     config = get_config()
 
-    db_read = mysql.connector.connect(
-        host=config.db_host,
-        port=config.db_port,
-        user=config.db_username,
-        password=config.db_password,
-        database=config.db_name,
-        autocommit=True,
-    )
+    db_read = get_mysql_connection(config)
     cursor_read = db_read.cursor(dictionary=True)
 
-    db_write = mysql.connector.connect(
-        host=config.db_host,
-        port=config.db_port,
-        user=config.db_username,
-        password=config.db_password,
-        database=config.db_name,
-        autocommit=True,
-    )
+    db_write = get_mysql_connection(config)
     cursor_write = db_write.cursor()
 
-    cursor_read.execute("SHOW COLUMNS FROM authors WHERE Field = 'normalized_name'")
-    normalized_name_column = cursor_read.fetchone()
-
-    if normalized_name_column is None:
-        cursor_write.execute("ALTER TABLE authors ADD normalized_name VARCHAR(500) DEFAULT NULL")
-
-    cursor_read.execute("SELECT * FROM authors order by id asc")
-    while True:
-        authors = cursor_read.fetchmany(1000)
-        if not authors:
-            break
-
-        normalized_authors = []
-        for author in authors:
-            name = normalizer.Normalizer.normalize(author["name"])
-            normalized_authors.append((name, author["id"]))
-
-        cursor_write.executemany(
-            "UPDATE authors SET normalized_name = %s WHERE id = %s",
-            normalized_authors
-        )
+    create_normalized_name_column(cursor_read, cursor_write)
+    populate_normalized_name_column(cursor_read, cursor_write)
 
     cursor_write.close()
     cursor_read.close()
@@ -73,6 +40,43 @@ def get_config():
     parser.add_argument("--db-name", type=str, default=db_name, help="Database name")
 
     return parser.parse_args()
+
+
+def get_mysql_connection(config):
+    return mysql.connector.connect(
+        host=config.db_host,
+        port=config.db_port,
+        user=config.db_username,
+        password=config.db_password,
+        database=config.db_name,
+        autocommit=True,
+    )
+
+
+def create_normalized_name_column(cursor_read, cursor_write):
+    cursor_read.execute("SHOW COLUMNS FROM authors WHERE Field = 'normalized_name'")
+    normalized_name_column = cursor_read.fetchone()
+
+    if normalized_name_column is None:
+        cursor_write.execute("ALTER TABLE authors ADD normalized_name VARCHAR(500) DEFAULT NULL")
+
+
+def populate_normalized_name_column(cursor_read, cursor_write):
+    cursor_read.execute("SELECT * FROM authors order by id asc")
+    while True:
+        authors = cursor_read.fetchmany(1000)
+        if not authors:
+            break
+
+        normalized_authors = []
+        for author in authors:
+            name = normalizer.Normalizer.normalize(author["name"])
+            normalized_authors.append((name, author["id"]))
+
+        cursor_write.executemany(
+            "UPDATE authors SET normalized_name = %s WHERE id = %s",
+            normalized_authors
+        )
 
 
 main()
