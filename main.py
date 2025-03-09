34288298ba691d280f0ac876b0f64288e1e791cc
Flavius Dranca <flavius.dranca@gmail.com>
2025-03-09 03:30:56 +0200

    Implement duplicate authors merge

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ def main():
     create_normalized_name_column(cursor_read, cursor_write)
     populate_normalized_name_column(cursor_read, cursor_write)
     index_normalized_name_column(cursor_read, cursor_write)
+    duplicated_authors = get_duplicated_authors(cursor_read)
+
+    for author in duplicated_authors:
+        main_author_id = get_main_author_id(cursor_read, author["normalized_name"])
+        merge_authors(cursor_read, cursor_write, author["normalized_name"], main_author_id)
+
+    delete_normalized_name_column(cursor_write)
 
     cursor_write.close()
     cursor_read.close()
@@ -86,6 +93,56 @@ def index_normalized_name_column(cursor_read, cursor_write):
 
     if normalized_name_index is None:
         cursor_write.execute("ALTER TABLE authors ADD INDEX normalized_name_index (normalized_name)")
+
+
+def get_duplicated_authors(cursor_read):
+    cursor_read.execute("""
+        SELECT normalized_name, COUNT(*) AS count
+        FROM authors
+        GROUP BY normalized_name
+        HAVING count > 1;
+    """)
+
+    return cursor_read.fetchall()
+
+
+def get_main_author_id(cursor_read, normalized_name):
+    cursor_read.execute(
+        """
+            SELECT author_book.author_id, COUNT(*) AS books_count
+            FROM authors
+            LEFT JOIN author_book ON author_book.author_id = authors.id
+            WHERE authors.normalized_name = %s
+            GROUP BY author_book.author_id
+            ORDER BY books_count DESC, author_book.author_id ASC
+            LIMIT 1
+        """,
+        [normalized_name],
+    )
+
+    return cursor_read.fetchone()["author_id"]
+
+
+def merge_authors(cursor_read, cursor_write, normalized_name, main_author_id):
+    cursor_read.execute(
+        "SELECT id FROM authors WHERE normalized_name = %s AND id != %s",
+        [normalized_name, main_author_id],
+    )
+    authors = cursor_read.fetchall()
+
+    cursor_write.executemany(
+        "UPDATE author_book SET author_id = %s WHERE author_id = %s",
+        [(main_author_id, author["id"]) for author in authors]
+    )
+
+    cursor_write.execute(
+        "DELETE FROM authors WHERE normalized_name = %s AND id != %s",
+        [normalized_name, main_author_id],
+    )
+
+
+def delete_normalized_name_column(cursor_write):
+    cursor_write.execute("ALTER TABLE authors DROP COLUMN normalized_name")
 
 
 main()
